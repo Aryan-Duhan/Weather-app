6cdb4f5879d83d0d4bcde453fa848704114e1313
Aryan-Duhan <aryanduhan44@gmail.com>
2025-08-26 00:25:58 +0530

    UI for app

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -17,8 +17,24 @@ const Weather = () => {
             <img src={search_icon} alt="" />
         </div>  
         <img src={clear_icon} alt="" className='weather_icon' />
-        <p>16°C</p>
-        <p>London</p>
+        <p className='temperature'>16°C</p>
+        <p className='location'>London</p>
+        <div className="weather-data">
+            <div className="col">
+                <img src={humidity_icon} alt="" />
+                <div>
+                    <p>91 %</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+            <div className="col">
+                <img src={wind_icon} alt="" />
+                <div>
+                    <p>3.6 km/h</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
